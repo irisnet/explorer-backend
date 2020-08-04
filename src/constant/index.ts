6811795797be56b50a61b135b7b9bdf594adebca
hangTaishan <hangTaishan@bishuilantiandeMacBook-Pro.local>
2020-08-04 10:18:29 +0800

    #tx type

--- src/constant/index.ts
+++ src/constant/index.ts
@@ -19,29 +19,73 @@ export enum ENV {
 };
 
 export enum TxType {
-    create_record = 'CreateRecord',
-    mint_token = 'MintToken',
-    burn_nft = 'BurnNft',
-    send = 'Send',
-    respond_service = 'RespondService',
-    transfer_nft = 'TransferNft',
-    edit_nft = 'EditNft',
-    define_service = 'DefineService',
-    bind_service = 'BindService',
-    call_service = 'CallService',
-    issue_denom = 'IssueDenom',
-    mint_nft = 'MintNft',
-    transfer_token_owner = 'TransferTokenOwner',
-    issue_token = 'IssueToken',
-    edit_token = 'EditToken',
-    update_service_binding = 'UpdateServiceBinding',
-    disable_service_binding = 'DisableServiceBinding',
-    enable_service_binding = 'EnableServiceBinding',
-    refund_service_deposit = 'RefundServiceDeposit',
-    pause_request_context = 'PauseRequestContext',
-    start_request_context = 'StartRequestContext',
-    kill_request_context = 'KillRequestContext',
-    update_request_context = 'UpdateRequestContext',
+    // service
+    define_service = 'define_service',
+    bind_service = 'bind_service',
+    call_service = 'call_service',
+    respond_service = 'respond_service',
+    update_service_binding = 'update_service_binding',
+    disable_service_binding = 'disable_service_binding',
+    enable_service_binding = 'enable_service_binding',
+    refund_service_deposit = 'refund_service_deposit',
+    pause_request_context = 'pause_request_context',
+    start_request_context = 'start_request_context',
+    kill_request_context = 'kill_request_context',
+    update_request_context = 'update_request_context',
+    create_record = 'create_record',
+    // nft
+    burn_nft = 'burn_nft',
+    transfer_nft = 'transfer_nft',
+    edit_nft = 'edit_nft',
+    issue_denom = 'issue_denom',
+    mint_nft = 'mint_nft',
+    // Token
+    issue_token = 'issue_token',
+    edit_token = 'edit_token',
+    mint_token = 'mint_token',
+    transfer_token_owner = 'transfer_token_owner',
+    //Bank
+    send = 'send',
+    multisend = 'multisend',
+    //Crisis
+    verify_invariant = 'verify_invariant',
+    //Evidence
+    submit_evidence = 'submit_evidence',
+    //Staking
+    begin_unbonding = 'begin_unbonding',
+    edit_validator = 'edit_validator',
+    create_validator = 'create_validator',
+    delegate = 'delegate',
+    begin_redelegate = 'begin_redelegate',
+    // Slashing
+    unjail = 'unjail',
+    // Distribution
+    set_withdraw_address = 'set_withdraw_address',
+    withdraw_delegator_reward = 'withdraw_delegator_reward',
+    withdraw_validator_commission = 'withdraw_validator_commission',
+    fund_community_pool = 'fund_community_pool',
+    // Gov
+    deposit = 'deposit',
+    vote = 'vote',
+    submit_proposal = 'submit_proposal',
+    // Coinswap
+    add_liquidity = 'add_liquidity',
+    remove_liquidity = 'remove_liquidity',
+    swap_order = 'swap_order',
+    // Htlc
+    create_htlc = 'create_htlc',
+    claim_htlc = 'claim_htlc',
+    refund_htlc = 'refund_htlc',
+    // Guardian
+    add_profiler = 'add_profiler',
+    delete_profiler = 'delete_profiler',
+    add_trustee = 'add_trustee',
+    delete_trustee = 'delete_trustee',
+    // Oracle
+    create_feed = 'create_feed',
+    start_feed = 'start_feed',
+    pause_feed = 'pause_feed',
+    edit_feed = 'edit_feed',
 }
 
 export enum TxStatus {
